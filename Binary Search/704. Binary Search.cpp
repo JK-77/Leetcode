cc72fdf69e3f59234b039cd59a14ad0ec9b329e9
JK-77 <Jayeshkriplani9@gmail.com>
2024-09-22 01:27:59 +0530

    Update 704. Binary Search.cpp

--- Binary Search/704. Binary Search.cpp
+++ Binary Search/704. Binary Search.cpp
@@ -1,3 +1,4 @@
+// Iterative Approch 
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -9,5 +10,22 @@ public:
             else h = m - 1;
         }
         return -1;
+    }
+};
+
+// Recursion
+class Solution {
+public:
+
+    int bs(vector<int> &nums, int low, int high, int target) {
+        if (low > high) return -1;
+        int mid = low + (high - low) / 2;
+        if(nums[mid] == target) return mid;
+        else if(target > nums[mid]) return bs(nums, mid + 1, high, target);
+        return bs(nums, low, mid - 1, target);
+    }
+
+    int search(vector<int>& nums, int target) {
+        return bs(nums, 0, nums.size() - 1, target);
     }
 };
