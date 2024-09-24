f1fd1b3a4b93998421f0bef0a29ef877b108fb1c
JK-77 <Jayeshkriplani9@gmail.com>
2024-09-25 04:21:35 +0530

    Create 27. Remove Element.cpp

--- Arrays/27. Remove Element.cpp
+++ Arrays/27. Remove Element.cpp
@@ -1,1 +1,14 @@
-
+// TC: O(N)    SC: O(1)
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int cnt = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != val) {
+                nums[cnt++] = nums[i];
+            }
+        }
+        return cnt;
+    }
+};
