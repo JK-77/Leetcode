e737e400e89e4cdbfc1c2135d39e3324d37ba4a3
Jayesh Kriplani <Jayeshkriplani9@gmail.com>
2025-05-20 02:04:04 +0530

    Update 386. Lexicographical Numbers.cpp

--- 386. Lexicographical Numbers.cpp
+++ 386. Lexicographical Numbers.cpp
@@ -12,7 +12,7 @@ x
 
         for(int append = 0; append <= 9; append++) {
             int newNum = curr * 10 + append;x
- 
+ x
             if(newNum > n) {
                 return;
             }
